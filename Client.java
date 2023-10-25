4c41e460612c28362d674a9e637797e088f84237
TobyDook <c2020110@ncl.ac.uk>
2023-10-25 17:34:02 +0100

    Set sq is correct with alternating 0,1. Cleared buffer after each byte to avoid concatenation on smaller byte chunks less than 4

--- Client.java
+++ Client.java
@@ -5,28 +5,28 @@ import java.util.Scanner;
 
 public class Client {
 	DatagramSocket socket;
-	static final int RETRY_LIMIT = 4;	/* 
-	 * UTILITY METHODS PROVIDED FOR YOU 
+	static final int RETRY_LIMIT = 4;	/*
+	 * UTILITY METHODS PROVIDED FOR YOU
 	 * Do NOT edit the following functions:
 	 *      exitErr
 	 *      checksum
 	 *      checkFile
-	 *      isCorrupted  
-	 *      
+	 *      isCorrupted
+	 *
 	 */
 
 	/* exit unimplemented method */
 	public void exitErr(String msg) {
 		System.out.println("Error: " + msg);
 		System.exit(0);
-	}	
+	}
 
 	/* calculate the segment checksum by adding the payload */
 	public int checksum(String content, Boolean corrupted)
 	{
-		if (!corrupted)  
+		if (!corrupted)
 		{
-			int i; 
+			int i;
 			int sum = 0;
 			for (i = 0; i < content.length(); i++)
 				sum += (int)content.charAt(i);
@@ -41,25 +41,25 @@ public class Client {
 	{
 		File file = new File(fileName);
 		if(!file.exists()) {
-			System.out.println("SENDER: File does not exists"); 
-			System.out.println("SENDER: Exit .."); 
+			System.out.println("SENDER: File does not exists");
+			System.out.println("SENDER: Exit ..");
 			System.exit(0);
 		}
 		return file;
 	}
 
 
-	/* 
-	 * returns true with the given probability 
-	 * 
-	 * The result can be passed to the checksum function to "corrupt" a 
-	 * checksum with the given probability to simulate network errors in 
-	 * file transfer 
+	/*
+	 * returns true with the given probability
+	 *
+	 * The result can be passed to the checksum function to "corrupt" a
+	 * checksum with the given probability to simulate network errors in
+	 * file transfer
 	 */
 	public boolean isCorrupted(float prob)
-	{ 
-		double randomValue = Math.random();   
-		return randomValue <= prob; 
+	{
+		double randomValue = Math.random();
+		return randomValue <= prob;
 	}
 
 
@@ -94,47 +94,47 @@ public class Client {
 		System.out.println ("----------------------------------------------------");
 		String choice=args[4];
 		float loss = 0;
-		Scanner sc=new Scanner(System.in);  
+		Scanner sc=new Scanner(System.in);
 
 
 		System.out.println ("SENDER: Sending meta data");
-		client.sendMetaData(portNumber, ip, file, outputFile); 
+		client.sendMetaData(portNumber, ip, file, outputFile);
 
 		if (choice.equalsIgnoreCase("wt")) {
 			System.out.println("Enter the probability of a corrupted checksum (between 0 and 1): ");
 			loss = sc.nextFloat();
-		} 
+		}
 
 		System.out.println("------------------------------------------------------------------");
 		System.out.println("------------------------------------------------------------------");
 		switch(choice)
 		{
-		case "nm":
-			client.sendFileNormal (portNumber, ip, file);
-			break;
+			case "nm":
+				client.sendFileNormal (portNumber, ip, file);
+				break;
 
-		case "wt": 
-			client.sendFileWithTimeOut(portNumber, ip, file, loss);
-			break; 
-		default:
-			System.out.println("Error! mode is not recognised");
-		} 
+			case "wt":
+				client.sendFileWithTimeOut(portNumber, ip, file, loss);
+				break;
+			default:
+				System.out.println("Error! mode is not recognised");
+		}
 
 
 		System.out.println("SENDER: File is sent\n");
-		sc.close(); 
+		sc.close();
 	}
 
 
 	/*
 	 * THE THREE METHODS THAT YOU HAVE TO IMPLEMENT FOR PART 1 and PART 2
-	 * 
-	 * Do not change any method signatures 
+	 *
+	 * Do not change any method signatures
 	 */
 
-	/* TODO: send metadata (file size and file name to create) to the server 
+	/* TODO: send metadata (file size and file name to create) to the server
 	 * outputFile: is the name of the file that the server will create
-	*/
+	 */
 	public void sendMetaData(int portNumber, InetAddress IPAddress, File file, String outputFile) {
 		//exitErr("sendMetaData is not implemented");
 
@@ -166,7 +166,7 @@ public class Client {
 				throw new RuntimeException(e);
 			}
 			totalBits++;
-        }
+		}
 		// READ IN file length using stream somehow
 		try {
 			fileInputStream.close();
@@ -178,7 +178,7 @@ public class Client {
 		MetaData metaData = new MetaData();
 		metaData.setName("output.txt");
 		metaData.setSize(totalBits);
-	//	System.out.println(metaData.getSize());
+		//	System.out.println(metaData.getSize());
 
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		ObjectOutputStream os = null;
@@ -207,27 +207,54 @@ public class Client {
 
 
 	/* TODO: Send the file to the server without corruption*/
-	public void sendFileNormal(int portNumber, InetAddress IPAddress, File file) throws IOException {
+	public void sendFileNormal(int portNumber, InetAddress IPAddress, File file) {
 		//exitErr("sendFileNormal is not implemented");
-		    FileInputStream fileInputStream = new FileInputStream(file);
-			byte[] buffer = new byte[1024]; //reads 4 bytes at one time
-            int bytesRead = 0;
-            for 
-            bytesRead = fileInputStream.read(buffer);
+		try (FileInputStream fileInputStream = new FileInputStream(file)) {
+			byte[] buffer = new byte[4]; //reads 4 bytes at one time
+			int bytesRead; // = fileInputStream.read(buffer);
+			int sequenceCount=0;
 
-            if (bytesRead == 4) {
-                //System.out.println("Bytes read"+ bytesRead);
-                int asciiSum = 0;
-                for (byte myByte : buffer) {
-                    int asciiValue = myByte; // Convert byte array to ASCII value
-                    asciiSum += asciiValue;
-                }
-               // System.out.println("ASCII SUM LOOP"+asciiSum);
 
-				String text = new String(buffer, StandardCharsets.UTF_8); //converting byte array to string to load payload
+
+
+			//	byteArrayOutputStream.write(buffer,0,buffer.length);
+
+			while((bytesRead = fileInputStream.read(buffer)) != -1) {
+				//buffer
+				// int asciiOfA = (int) buffer;
+				// byte[] buffer = "Java".getBytes(StandardCharsets.US_ASCII);
+				//System.out.println(buffer);
+
+				System.out.println("Bytes read"+ bytesRead);
+
+
+				//byte myByte = buffer[0]; // Replace 'A' with the byte you want to convert
+				// int asciiValue = (int) myByte;
+				//  System.out.println("ASCII Value: " + asciiValue);
+
+				int asciiSum = 0;
+
+				for (byte myByte : buffer) {
+					int asciiValue = myByte; // Convert to ASCII value
+					asciiSum += asciiValue;
+
+				}
+				System.out.println("ASCII SUM LOOP"+asciiSum);
+
+				String text = new String(buffer, StandardCharsets.UTF_8);
+				byte[] bytes = text.getBytes("US-ASCII");
+				System.out.println("Byte ascii sum"+ bytes);
 				Segment seg0 = new Segment();
 				seg0.setPayLoad(text);
-				seg0.setSq(0);
+
+				if (sequenceCount % 2 == 0) {
+					seg0.setSq(0);
+				}
+				else{
+					seg0.setSq(1);
+				}
+				sequenceCount++;
+
 				seg0.setSize(buffer.length);
 				seg0.setType(SegmentType.Data);
 				seg0.setChecksum(asciiSum);
@@ -239,28 +266,39 @@ public class Client {
 
 				DatagramPacket sentPacket = new DatagramPacket(byteArray, byteArray.length, IPAddress, portNumber);
 				socket.send(sentPacket);
-				socket.close();
+				buffer = new byte[4];
 
 
-			//	System.out.println(seg0.getChecksum());
-			//	System.out.println(seg0);
-			//	System.out.println(seg0.getPayLoad());
-			//	System.out.println(seg0.getSq());
-			//	System.out.println(seg0.getSize());
+
+
+
+				System.out.println(seg0.getChecksum());
+				System.out.println(seg0);
+				System.out.println(seg0.getPayLoad());
+				System.out.println(seg0.getSq());
+				System.out.println(seg0.getSize());
+
 
 			}
+			socket.close();
 
-    }
+			//System.out.println(loadedSegment);
+			//loadedSegment.setChecksum(4);
+		}catch(IOException e){
+			throw new RuntimeException(e);
+		}
 
 
+		//	checksum()
 
 
+	}
 	/* TODO: This function is essentially the same as the sendFileNormal function
-	 *      except that it resends data segments if no ACK for a segment is 
+	 *      except that it resends data segments if no ACK for a segment is
 	 *      received from the server.*/
 	public void sendFileWithTimeOut(int portNumber, InetAddress IPAddress, File file, float loss) {
 		exitErr("sendFileWithTimeOut is not implemented");
-	} 
+	}
 
 
 }
